7d2742e1e113e3d788fc7a9df8bb04413d779387
michael <michael@michael>
2019-03-01 20:59:30 -0600

    Fix broken search page and add books/results.html back after it got deleted by someone's push who didn't git pull

--- webapp/backend/flaskr/search.py
+++ webapp/backend/flaskr/search.py
@@ -1,14 +1,14 @@
-from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
-)
-
+# -*- coding: utf-8 -*-
 import functools
 
 from flaskr.db import get_db
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
 
-bp = Blueprint('search', __name__, url_prefix='/search')
+bp = Blueprint('search', __name__)
 
-@bp.route('/', methods=('GET', 'POST'))
+@bp.route('/search', methods=['POST'])
 def search():
     if request.method == 'POST':
         search_param = request.form['q']
