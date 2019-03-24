8562241d16c7e30c19743a4b14031a669bd75419
Nabilliban14 <nabilliban14@gmail.com>
2019-03-24 15:46:53 -0500

    Incorporated Autocomplete for author and title search. Had to move jQuery import out of base.html and into index.html because errors would not be resolved. Added /search/names endpoint into search.py file for autocomplete function.

--- webapp/backend/flaskr/search.py
+++ webapp/backend/flaskr/search.py
@@ -3,7 +3,7 @@ import functools
 
 from flaskr.db import get_db
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
+    Blueprint, flash, g, redirect, render_template, request, session, url_for, jsonify
 )
 
 bp = Blueprint('search', __name__)
@@ -34,3 +34,40 @@ def search():
 
         books = books_by_author + books_by_title
     return render_template('book/results.html', books=books)
+
+@bp.route('/search/names', methods=('GET', 'POST'))
+def autocomplete():
+    if request.method == 'GET':
+        search_param = request.args.get('term')
+        category = request.args.get('cat')
+        cur = get_db().cursor()
+        error = None
+        if category == 'Title':
+            cur.execute(
+                'SELECT TITLE FROM books WHERE books.title LIKE %s', ('%' + search_param + '%',)
+            )
+
+            books_by_title = cur.fetchall()
+            titles=[]
+            for book_title in books_by_title:
+                book = book_title['TITLE']
+                if book != 'title':
+                    titles.append(book)
+
+            return jsonify(matching_results=titles)
+
+        elif category == 'Author':
+            cur.execute(
+                'SELECT AUTHOR FROM books WHERE books.author LIKE %s', ('%' + search_param + '%',)
+            )
+
+	    books_by_author = cur.fetchall()
+            authors = []
+            for book_author in books_by_author:
+                author = book_author['AUTHOR']
+                if author != 'author':
+                    authors.append(author)
+
+            return jsonify(matching_results=authors)
+
+    return jsonify(matching_results=[])
